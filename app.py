c10ca923054916a5d8e4c45027ffd311d058f0fb
topoexplore-del <topoexplore@gmail.com>
2026-03-01 21:32:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ st.markdown("""
         color: white;
         text-align: center;
         width: 100%;
-        max-width: 400px;
+        max-width: 700px;
         margin: 0 auto;
         display: block;
     }
@@ -179,6 +179,7 @@ csv = filtered.to_csv(index=False).encode('utf-8')
 st.download_button("📥 Descargar resultados como CSV", csv, "secop_resultados.csv", "text/csv")
 
 st.success("✅ Dashboard cargado correctamente.")
+
 
 
 
