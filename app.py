2bb6d399922c8ebb87076f1b62811f0613816394
topoexplore-del <topoexplore@gmail.com>
2026-03-01 21:47:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ st.markdown("""
         width: 100%;
         max-width: 400px;
         display: block;
-        margin-left: auto;
+        margin-bottom: auto;
         margin-right: auto;
         transition: all 0.3s;
     }
@@ -179,6 +179,7 @@ csv = filtered.to_csv(index=False).encode('utf-8')
 st.download_button("📥 Descargar resultados como CSV", csv, "secop_resultados.csv", "text/csv")
 
 st.success("✅ Dashboard cargado correctamente.")
+
 
 
 
